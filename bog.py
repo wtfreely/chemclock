cbb239082eff8271731cc08a9d8cd917681ecf62
Warren <wjtemple@gmail.com>
2022-04-18 19:03:45 -0700

    updated

--- bog.py
+++ bog.py
@@ -8,11 +8,6 @@ It sure made dealing with this a lot easier.
 import os
 import sys
 import random
-import datetime
-import locale
-locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-today = datetime.date.today()
-
 import pubchempy as pcp
 from omni_epd import displayfactory, EPDNotFoundError
 from PIL import Image, ImageDraw, ImageFont
@@ -50,19 +45,15 @@ def get_chem(min, max):
     while good_chem == False:
         try:
             c = pcp.Compound.from_cid(n)
-            print(c.synonyms)
             formula = c.molecular_formula
             name = c.iupac_name
-            synonyms = c.synonyms
-            weight = c.molecular_weight
-            complexity = c.complexity
             pcp.download('PNG', 'chem.png', n, overwrite=True, size=400)
             good_chem = True
-            return formula, name, synonyms, weight, complexity
+            return formula, name
         except Exception as e:
             print(e)
             good_chem = False
-            print("Error.  Possible bad chemical or lack of internet?")
+            print("bad chem")
 
 """
 So, pubchems output is a little janky, and off center.
@@ -92,66 +83,35 @@ def image_processing(image):
 
     res = img.crop(bbox)
     res.save("processed.png", "png")
-    print("Saved processed image.")
+    print("saved!")
 
 def rectangle(x, y, radius, rgb):
     image = Image.new("RGBA", (x, y), rgb)
     draw = ImageDraw.Draw(image)
 
-    draw.rounded_rectangle((0, x, 0, y), fill=rgb, outline=None, width=1, radius=radius)
+    draw.rounded_rectangle((x, x, y, y), fill=rgb, radius=radius)
     return image
 
-def drawstuff(name):
+def drawstuff():
 
     """
     Let's make a white background, add some black bars for status stuff,
     And add a rectangular black box for the chemical
     Then we can jank-paste it all together.
     Maybe a loop!
-    Maybe not.
+    maybe not.
     """
  
-    ### Defining our canvas size since we use these values for a lot of math.
-
     epd.width = 800
     epd.height = 480
 
-    ### Molecules look a lot nicer with subscript, don't you think?
-
-    SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
-
-    molecule_formula = name[0].translate(SUB)
-    molecule_name = name[1]
-    molecule_synonyms = name[2]
-    molecule_weight = name[3]
-    molecule_complexity = name[4]
-
-    ### Let's make out background just a nice, white rectangle
-    ### And define our fonts :)
-
     background = rectangle(800, 480, 0, "white")
-    header = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 28)
-    paragraph = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSansBold.ttf", 22)
-
-    ### Our title bar can go at the top
-    ### And get a nice white title on it :)
-
     title_bar = rectangle(800, 40, 0, "black")
-    d = ImageDraw.Draw(title_bar)
-    d.multiline_text((10, 3), str("voilà chemclock.py"), font=header, fill=(255, 255, 255))
 
-    ### And a status bar can have the date
-
-    status_bar = rectangle(800, 40, 0, "black")
-    d = ImageDraw.Draw(status_bar)
-    d.multiline_text((10, 3), today.strftime("molécule du jour pour le %d de %B, %Y"), font=header, fill=(255, 255, 255))
-
-    ### Add 'em to the background!
-    
     background.paste(title_bar, (0, 0), title_bar)
-    background.paste(status_bar, (0, 440), title_bar)
+    background.paste(title_bar, (0, 440), title_bar)
 
-    ### Now let's draw the chemical structure
+
 
     with Image.open("processed.png") as chemical:
         width, height = chemical.size
@@ -166,22 +126,32 @@ def drawstuff(name):
     chemical_bg_y = int((epd.height - height - chemical_border)/2)
     background.paste(chemical_bg, (40, chemical_bg_y), chemical_bg)
 
-    ### oKAY now let's draw some actual text for the thingy okay :)
+    # background = rectangle(800, 480, 0, "#FFFFFF")
 
-    d = ImageDraw.Draw(background)
-    d.multiline_text((width + chemical_border + 75, 110), str(f"formule moléculaire: %su" % (molecule_formula)), font=paragraph, fill=(000, 000, 00))
+    # #background =  Image.open("bg.png")
+    # with Image.open("processed.png") as chemical:
+    #     width, height = chemical.size
 
-    if len(molecule_synonyms) == 0:
-        d = ImageDraw.Draw(background)
-        d.multiline_text((width + chemical_border + 75, 190), str(f"pas des synonymes connus."), font=paragraph, fill=(000, 000, 00))
-    elif len(molecule_synonyms) > 0:
-        d = ImageDraw.Draw(background)
-        d.multiline_text((width + chemical_border + 75, 190), str(f"synonyme connu: %s" % (molecule_synonyms[0])), font=paragraph, fill=(000, 000, 00))
+    # chemical = Image.open("processed.png")
 
 
-    d.multiline_text((width + chemical_border + 75, 270), str(f"masse moléculaire: %su" % (molecule_weight)), font=paragraph, fill=(000, 000, 00))
-    d.multiline_text((width + chemical_border + 75, 350), str(f"évaluation de la complexité: %s" % (molecule_complexity)), font=paragraph, fill=(000, 000, 00))
+    # chemical_rect = rectangle(height, width, 50, "#000000")
+    # chemical_rect.paste(chemical, (0,0), chemical_rect)
 
+    # box_height = int((height/2))
+
+    # chemicalbackground.paste(chemical_rect, (0,0), chemical_rect)
+    # background.show()
+
+
+    # # font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 40)
+    # # d = ImageDraw.Draw(background)
+    # # d.multiline_text((10, 10), "Hello\nWorld AAAAAA", font=font, fill=(125, 125, 125))
+
+
+
+    epd.width = 800
+    epd.height = 480
     epd.prepare()
     epd.clear()
     epd.display(background)
@@ -191,10 +161,10 @@ def drawstuff(name):
 
 
 def main():
-    print("Running main()  ")
-    molecule = get_chem(1, 10000)
+    print("Running main()   ")
+    get_chem(1, 10000)
     image_processing("chem.png")
-    drawstuff(molecule)
+    drawstuff()
 
 if __name__ == '__main__':
     main()
